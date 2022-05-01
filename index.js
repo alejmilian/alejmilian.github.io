efeba7c9a12f4cabbe05be0d1be83e1500b4940a
alejmilian <amilianmoron@gmail.com>
2022-04-30 23:04:51 -0400

    Start using Bulma

---
+++ index.js
@@ -0,0 +1,4 @@
+document.querySelector(".navbar-burger").addEventListener("click", (e) => {
+  document.querySelector(".navbar-burger").classList.toggle("is-active");
+  document.querySelector(".navbar-menu").classList.toggle("is-active");
+});
